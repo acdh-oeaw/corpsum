5e9991699c1b5892e8f06efd1ad16df71ea04ebf
Asil Çetin <25176039+asilcetin@users.noreply.github.com>
2019-04-11 15:18:44 +0200

    basic pie charts on map

--- src/components/d3-charts/map.vue
+++ src/components/d3-charts/map.vue
@@ -20,6 +20,7 @@ export default {
     // Initial mounting
     this.drawMapPaths().then((response) => {
       this.geoMapData = response;
+      this.appendMapData(this.mapData, this.geoMapData);
     });
   },
   watch: {
@@ -61,13 +62,12 @@ export default {
       return geoMapData;
     },
     appendMapData(mapData, geoMapData) {
-      console.log(mapData);
 
       d3.selectAll(".pie-country").remove();
 
       const countries = [
-        { id: 32, name: "Argentina" },
-        { id: 68, name: "Bolivia" },
+        { id: '032', name: "Argentina" },
+        { id: '068', name: "Bolivia" },
         { id: 152, name: "Chile" },
         { id: 170, name: "Colombia" },
         { id: 188, name: "Costa_Rica" },
@@ -102,7 +102,7 @@ export default {
 
       const maxValue = d3.max(mapData, function(d) { return d.value });
       const colorScale = d3.scaleLinear().domain([0, maxValue]).range(['beige', 'red']);
-      const sizeScale = d3.scaleLinear().domain([0, maxValue]).range([1, 10]);
+      const sizeScale = d3.scaleLinear().domain([0, maxValue]).range([10, 50]);
       const ordinalColorScale = d3.scaleOrdinal(d3.schemeCategory10);
       const ordinalPositionScale = d3.scaleOrdinal().range([0, 20, 40, 60]);
 
@@ -117,6 +117,103 @@ export default {
           return '#ddd';
         })
         */
+
+/*
+      const pie = d3.pie()
+          .value(d => d.value)
+          .sort(null);
+
+      const arc = d3.arc()
+          .innerRadius(0)
+          .outerRadius(radius);
+*/
+
+
+      function groupBy(collection, property) {
+          var i = 0, val, index,
+              values = [], result = [];
+          for (; i < collection.length; i++) {
+              val = collection[i][property];
+              index = values.indexOf(val);
+              if (index > -1)
+                  result[index].push(collection[i]);
+              else {
+                  values.push(val);
+                  result.push([collection[i]]);
+              }
+          }
+          return result;
+      }
+
+      var mapData2 = groupBy(mapData, "key");
+
+      var width = 50,
+          height = 50,
+          radius = Math.min(width, height) / 2;
+
+      var color = d3.scaleOrdinal()
+          .range(["#98abc5", "#8a89a6", "#7b6888"]);
+
+      var arc = d3.arc()
+          .outerRadius(radius - 10)
+          .innerRadius(0);
+
+      var labelArc = d3.arc()
+          .outerRadius(radius - 10)
+          .innerRadius(radius - 10);
+
+      var pie = d3.pie()
+          .sort(null)
+          .value(function(d) { return d.value; });
+
+
+      var countryPies = d3.select(".map-group").selectAll(".country-pie")
+        .data(mapData2)
+        .enter()
+        .append("g")
+        .attr("class", function(d) { return "country-pie country-pie-"+d[0].id; })
+        // using the map data
+        // position a circle for matches in mapData array
+        .attr("transform", function(d) {
+          var element = document.getElementById("country-"+d[0].id);
+          if (element) {
+            var boundingBox = element.getBBox();
+            var x = Math.round(boundingBox.x) + ordinalPositionScale(d[0].group);
+            var y = Math.round(boundingBox.y);
+            return "translate("+x+","+y+")";
+          }
+        })
+
+        var arcs = countryPies
+        .selectAll(".country-arc")
+        .data(function(d) { return pie(d) })
+        .enter()
+        .append("g")
+        .attr("class", function(d, i) { return "arc arc-" + d.data.group; })
+
+        arcs.append("path")
+        .attr("d", arc)
+        .attr("fill", function(d) {
+          return ordinalColorScale(d.data.group);
+        })
+        .on("mouseover", function(d) {
+          d3.select(this.parentNode).selectAll("text")
+          .style("display", "block");
+        })
+        .on("mouseout", function(d) {
+          d3.select(this.parentNode).selectAll("text")
+          .style("display", "none");
+        })
+
+        arcs.append("text")
+        .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .style("display", "none")
+        .text(function(d) { return d.data.group; });
+
+/*
+        .data(pie(function(d) { console.log(d); return d.value; }))
+
 
       var pies = d3.select(".map-group").selectAll("circle")
         .data(mapData)
@@ -143,7 +240,7 @@ export default {
             return "translate("+x+","+y+")";
           }
 
-          /*
+          
           const paths = geoMapData;
           console.log(paths);
           for (var i = 0; i < paths.data().length; i++){
@@ -153,8 +250,9 @@ export default {
               t[0] = t[0] + ordinalPositionScale(d.group);
               return "translate(" + t + ")";
             }
-          }*/
+          }
         });
+      */
     }
   }
 };
